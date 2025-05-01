73e801968c1ebb8f9be4faaef87770d446a07712
RaedYassin <raedyassin02468@gmail.com>
2025-05-01 19:11:34 +0300

    fix(deployment): cookies don't ad

--- vite.config.js
+++ vite.config.js
@@ -15,8 +15,6 @@ export default defineConfig({
     proxy: {
       '/api/': 'http://localhost:5222',
       "/uploads/": "http://localhost:5222", 
-      changeOrigin: true,
-      secure: true,
     },
   }
 })
