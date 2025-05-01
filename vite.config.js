5bb5687533ca024cec40cd03b43d8a4c4752cd9d
RaedYassin <raedyassin02468@gmail.com>
2025-05-01 14:40:32 +0300

    add live backend URL

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// import flowbite from 'flowbite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    // flowbite.plugin(),
   ],
   server: {
     proxy: {
-      '/api/': 'http://localhost:5222',
-      "/uploads/": "http://localhost:5222", 
+      '/api/': 'https://cloud-dream-ecommerce-backend-production.up.railway.app/',
+      "/uploads/": "https://cloud-dream-ecommerce-backend-production.up.railway.app/", 
+      changeOrigin: true,
+      secure: true,
     },
   }
-  // content: [
-  //   flowbite.content(),
-  // ],
 
 })
